cdf6c0ec140b7792a108f4305ca0b2314bb75113
arjunmenon-IoT <p.arjun12527@gmail.com>
2021-12-28 15:46:42 +0100

    Final

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // options...
+    devServer: {
+        proxy: 'http://faircorp-arjun-puliyasseri-final.cleverapps.io/api/',
+    },
+    lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/' + process.env.CI_PROJECT_NAME + '/'
+        : '/'
+}
